3abfe3e6f9957748822555b04cc7465d3aca67d4
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-11-16 00:32:03 +0530

    testing-3

--- index.js
+++ index.js
@@ -7,32 +7,28 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const atlasConnectionUri = 'mongodb+srv://aaronphilip2003:Aaron123@cluster0.qhmzihy.mongodb.net/?retryWrites=true&w=majority';
-const dbName="subjects";
+const dbName = "subjects";
 
 mongoose.connect(atlasConnectionUri, {
-    dbName: 'subjects'
+  dbName: 'subjects'
 });
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB Atlas');
-});
 
-app.use(cors(
-    {
-        origin: ["https://mern-attendance-client.vercel.app","localhost:3000"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+  // Set up routes only after the MongoDB connection is established
 
-// app.use(cors())
+  app.use(cors({
+    origin: ["https://mern-attendance-client.vercel.app", "localhost:3000"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }));
 
-app.get("/", (req, res) => {
-  res.json("Hello");
-})
+  app.get("/", (req, res) => {
+    res.json("Hello");
+  });
 
-// Example route to fetch all "subjects" from the "subjects" collection
-mongoose.connection.on('connected', () => {
+  // Example route to fetch all "subjects" from the "subjects" collection
   app.get('/subjects', async (req, res) => {
     try {
       const db = mongoose.connection.db;
@@ -44,36 +40,31 @@ mongoose.connection.on('connected', () => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
-});
 
-app.get('/getattendance', async (req, res) => {
-  try {
-    const db = mongoose.connection.db;
-    const subjectsCollection = db.collection('attendances');
-    const attendanceStud = await subjectsCollection.find().toArray();
+  app.get('/getattendance', async (req, res) => {
+    try {
+      const db = mongoose.connection.db;
+      const subjectsCollection = db.collection('attendances');
+      const attendanceStud = await subjectsCollection.find().toArray();
 
-    console.log(attendanceStud)
+      console.log(attendanceStud);
 
-    res.json(attendanceStud);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+      res.json(attendanceStud);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-// Example route to update attendance for a subject
-app.post('/update-attendance-yes/:subject_name', async (req, res) => {
+  // Example route to update attendance for a subject
+  app.post('/update-attendance-yes/:subject_name', async (req, res) => {
     try {
       const subjectName = req.params.subject_name;
       const attended = req.params.attendance;
-  
+
       // Find the attendance record for the subject
       let attendanceRecord = await Attendance.findOne({ subject_name: subjectName });
-  
+
       // If the record doesn't exist, create a new one
       if (!attendanceRecord) {
         attendanceRecord = new Attendance({
@@ -81,26 +72,25 @@ app.post('/update-attendance-yes/:subject_name', async (req, res) => {
           attendance: { attended: 0, total: 0 },
         });
       }
-  
+
       // Ensure "attendance" field is defined
       if (!attendanceRecord.attendance) {
         attendanceRecord.attendance = { attended: 0, total: 0 };
       }
-  
+
       // Update attendance based on the "attended" parameter
       if (attended === 'yes') {
         attendanceRecord.attendance.attended += 1;
         attendanceRecord.attendance.total += 1;
-
       }
-  
+
       // Always increment the total
       attendanceRecord.attendance.total += 1;
       attendanceRecord.attendance.attended += 1;
-  
+
       // Save the updated attendance record
       await attendanceRecord.save();
-  
+
       res.json({ success: true, message: 'Attendance updated successfully' });
     } catch (error) {
       console.error(error);
@@ -108,43 +98,56 @@ app.post('/update-attendance-yes/:subject_name', async (req, res) => {
     }
   });
 
-app.post('/update-attendance-no/:subject_name', async (req, res) => {
-try {
-    const subjectName = req.params.subject_name;
-    const attended = req.params.attendance;
+  app.post('/update-attendance-no/:subject_name', async (req, res) => {
+    try {
+      const subjectName = req.params.subject_name;
+      const attended = req.params.attendance;
 
-    // Find the attendance record for the subject
-    let attendanceRecord = await Attendance.findOne({ subject_name: subjectName });
+      // Find the attendance record for the subject
+      let attendanceRecord = await Attendance.findOne({ subject_name: subjectName });
 
-    // If the record doesn't exist, create a new one
-    if (!attendanceRecord) {
-    attendanceRecord = new Attendance({
-        subject_name: subjectName,
-        attendance: { attended: 0, total: 0 },
-    });
+      // If the record doesn't exist, create a new one
+      if (!attendanceRecord) {
+        attendanceRecord = new Attendance({
+          subject_name: subjectName,
+          attendance: { attended: 0, total: 0 },
+        });
+      }
+
+      // Ensure "attendance" field is defined
+      if (!attendanceRecord.attendance) {
+        attendanceRecord.attendance = { attended: 0, total: 0 };
+      }
+
+      // Update attendance based on the "attended" parameter
+      if (attended === 'yes') {
+        attendanceRecord.attendance.attended += 1;
+        attendanceRecord.attendance.total += 1;
+      }
+
+      // Always increment the total
+      attendanceRecord.attendance.total += 1;
+
+      // Save the updated attendance record
+      await attendanceRecord.save();
+
+      res.json({ success: true, message: 'Attendance updated successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
+  });
 
-    // Ensure "attendance" field is defined
-    if (!attendanceRecord.attendance) {
-    attendanceRecord.attendance = { attended: 0, total: 0 };
-    }
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 
-    // Update attendance based on the "attended" parameter
-    if (attended === 'yes') {
-    attendanceRecord.attendance.attended += 1;
-    attendanceRecord.attendance.total += 1;
+});
 
-    }
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
 
-    // Always increment the total
-    attendanceRecord.attendance.total += 1;
-
-    // Save the updated attendance record
-    await attendanceRecord.save();
-
-    res.json({ success: true, message: 'Attendance updated successfully' });
-} catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-}
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
 });
